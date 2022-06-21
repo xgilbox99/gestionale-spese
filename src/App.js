99a1993acd0d268fad70b9f3308addbf5c7d299c
xgilbox99 <caparonejacopo99@gmail.com>
2022-06-21 15:22:46 +0200

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
-import logo from './logo.svg';
+import logo from './bg.png';
 import './App.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+      <header className="App-header p-4">
+        <img src={logo} className="App-logo my-5" alt="logo" />
+        <p className='mt-2'>
+          GESTIONALE SPESE "BG - GROUP"
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <table className='table table-info'>
+          <thead className='table-dark'>
+            <tr>
+              <th>Personaggio</th>
+              <th>Rimanenza</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>ALESSANDRO PERUZZO</td>
+              <td>€ 1,30</td>
+            </tr>
+            <tr>
+              <td>LEONARDO COSARO</td>
+              <td>€ 1,30</td>
+            </tr>
+            <tr>
+              <td>CLAUDIO DI PALMA</td>
+              <td>€ 1,30</td>
+            </tr>
+            <tr>
+              <td>JACOPO CAPARONE</td>
+              <td>€ 1,40</td>
+            </tr>
+          </tbody>
+          <tfoot>
+            <tr className='table-dark'>
+              <td>TOTALE SPESA</td>
+              <td>€ 5,30</td>
+            </tr>
+            <tr className='table-dark'>
+              <td>TOTALE RIMANENZA</td>
+              <td>€ 214,70</td>
+            </tr>
+          </tfoot>
+        </table>
       </header>
     </div>
   );
